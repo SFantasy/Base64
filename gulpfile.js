5576dbe31f53df4de8f88c2bf05670632684acb8
Fantasy Shao <forever.fantasy27@gmail.com>
2014-07-25 22:04:38 +0800

    Add gulp as task runner.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,33 @@
+// Module dependencies
 var gulp = require('gulp');
 
-gulp.task('default', function() {
+var coffee = require('gulp-coffee'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-ruby-sass');
 
+var paths = {
+    scripts: 'src/coffee/*.coffee',
+    sass: 'src/sass/*.sass'
+};
+
+gulp.task('default', ['scripts', 'sass']);
+
+gulp.task('scripts', function() {
+    gulp.src(paths.scripts)
+        .pipe(coffee())
+        .pipe(uglify())
+        .pipe(concat('main.min.js'))
+    .pipe(gulp.dest('dest/js'));
+});
+
+gulp.task('sass', function() {
+    gulp.src(paths.sass)
+        .pipe(sass({style: 'compressed', noCache: true}))
+    .pipe(gulp.dest('dest/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.scripts, ['scripts']);
+    gulp.watch(paths.sass, ['sass']);
 });
